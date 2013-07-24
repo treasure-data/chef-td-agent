9bde5e73d8e488d1b8f8def0a0695eed240c60f5
Michael H. Oshita <ijinpublic+github@gmail.com>
2013-07-24 16:19:45 +0900

    change copyright ownership

--- resources/gem.rb
+++ resources/gem.rb
@@ -4,7 +4,7 @@
 # 
 # Author:: Michael H. Oshita <ijinpublic+github@gmail.com>
 #
-# Copyright 2013, Michael H. Oshita
+# Copyright 2013, Treasure Data, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
