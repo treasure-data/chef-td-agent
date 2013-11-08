1ba63b71f6eb7b62b4e099c8dcc22b5cab15df35
Wen Hsiao <whsiao@optimisdev.com>
2013-11-08 11:27:57 +0800

    Changed mode of td-agent folders

--- recipes/optimis_config.rb
+++ recipes/optimis_config.rb
@@ -2,14 +2,14 @@
 directory '/var/log/td-agent' do
   owner  'td-agent'
   group  'td-agent'
-  mode   '0755'
+  mode   '0775'
   recursive true
 end
 
 directory '/var/run/td-agent' do
   owner  'td-agent'
   group  'td-agent'
-  mode   '0755'
+  mode   '0775'
   recursive true
 end
 
