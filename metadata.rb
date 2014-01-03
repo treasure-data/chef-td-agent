ec799fe8c597f31f157303e45d4ae639c38e6c1a
Wen Hsiao <whsiao@optimisdev.com>
2014-01-03 13:13:33 +0900

    Added out file hl7 log config to serviceRC

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "k@treasure-data.com"
 license          "All rights reserved"
 description      "Installs/Configures td-agent"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.1.4"
+version          "0.1.5"
 recipe           "td-agent", "td-agent configuration"
 
 %w{redhat centos amazon debian ubuntu}.each do |os|
