c59042725bc753e908e87bc90f46842afb6120c2
Yamashita Yuu <peek824545201@gmail.com>
2014-12-01 14:39:07 +0900

    Licensed under Apache License Version 2.0 explicitly (fixes #37)

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             "td-agent"
 maintainer       "Treasure Data, Inc."
 maintainer_email "k@treasure-data.com"
-license          "All rights reserved"
+license          "Apache 2.0"
 description      "Installs/Configures td-agent"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
