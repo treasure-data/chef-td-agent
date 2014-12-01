357e19baa68270251311ee88146b0ddbc21cd4a3
Yamashita Yuu <peek824545201@gmail.com>
2014-12-01 14:40:11 +0900

    Use `README.md` as the `long_description` of the cookbook

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       "Treasure Data, Inc."
 maintainer_email "k@treasure-data.com"
 license          "Apache 2.0"
 description      "Installs/Configures td-agent"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 recipe           "td-agent", "td-agent configuration"
 
