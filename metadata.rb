e3bf3499e6c75306e26eaed2c28d9aea52a738f8
Yamashita Yuu <peek824545201@gmail.com>
2014-12-07 02:00:53 +0900

    Release 1.0.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "k@treasure-data.com"
 license          "Apache 2.0"
 description      "Installs/Configures td-agent"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.3"
+version          "1.0.0"
 recipe           "td-agent", "td-agent configuration"
 
 %w{redhat centos debian ubuntu}.each do |os|
