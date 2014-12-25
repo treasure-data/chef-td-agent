3727089a5cd1b8e37ebadc3ad2bf02a11ed006af
Pavel Yudin <pyudin@parallels.com>
2014-12-25 18:22:51 +0300

    Lwrp for source, match and plugin added.

---
+++ providers/match.rb
@@ -0,0 +1,75 @@
+#
+# Cookbok Name:: td-agent
+# Provider:: match
+#
+# Author:: Pavel Yudin <pyudin@parallels.com>
+#
+#
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+use_inline_resources
+
+action :create do
+  fail 'You should set the node[:td_agent][:includes] attribute to true to use this resource.' unless node['td_agent']['includes']
+
+  service 'td-agent' do
+    action [ :nothing ]
+  end
+
+  template "/etc/td-agent/conf.d/#{new_resource.match_name}.conf" do
+    source 'match.conf.erb'
+    owner 'root'
+    group 'root'
+    mode '0644'
+    variables(type: new_resource.type,
+              params: params_to_text(new_resource.params),
+              tag: new_resource.tag)
+    cookbook 'td-agent'
+    notifies :reload, 'service[td-agent]'
+  end
+end
+
+action :delete do
+  service 'td-agent' do
+    action [ :nothing ]
+  end
+
+  file "/etc/td-agent/conf.d/#{new_resource.match_name}.conf" do
+    action :delete
+    only_if { ::File.exist?("/etc/td-agent/conf.d/#{new_resource.match_name}.conf") }
+    notifies :reload, 'service[td-agent]'
+  end
+end
+
+def params_to_text(params)
+  body = ''
+  Chef::Log.info(params)
+  params.each do |k,v|
+    if v.is_a?(Hash)
+      body += "<#{k}>\n"
+      body += params_to_text(v)
+      body += "</#{k}>\n"
+    elsif v.is_a?(Array)
+      v.each do |v|
+        body += "<#{k}>\n"
+        body += params_to_text(v)
+        body += "</#{k}>\n"
+      end
+    else
+      body += "#{k} #{v}\n"
+    end
+  end
+  body
+end
---
+++ providers/plugin.rb
@@ -0,0 +1,46 @@
+#
+# Cookbok Name:: td-agent
+# Provider:: plugin
+#
+# Author:: Pavel Yudin <pyudin@parallels.com>
+#
+#
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+use_inline_resources
+
+action :create do
+  directory '/etc/td-agent/plugin' do
+    owner "root"
+    group "root"
+    mode "0755"
+    action :create
+  end
+
+  remote_file "/etc/td-agent/plugin/#{new_resource.plugin_name}.rb" do
+    action :create_if_missing
+    owner 'root'
+    group 'root'
+    mode '0644'
+    source new_resource.url
+  end
+end
+
+action :delete do
+  file "/etc/td-agent/plugin/#{new_resource.plugin_name}.rb" do
+    action :delete
+    only_if { ::File.exist?("/etc/td-agent/plugin/#{new_resource.plugin_name}.rb") }
+  end
+end
