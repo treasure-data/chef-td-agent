5b7fd30aa37bacc9817f99479f13367ac24cc0a7
Yamashita, Yuu <peek824545201@gmail.com>
2015-08-28 10:20:45 +0900

    Bump version for development; 2.1.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "k@treasure-data.com"
 license          "Apache 2.0"
 description      "Installs/Configures td-agent"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "2.1.0"
+version          "2.1.1"
 recipe           "td-agent", "td-agent configuration"
 
 %w{redhat centos debian ubuntu}.each do |os|
