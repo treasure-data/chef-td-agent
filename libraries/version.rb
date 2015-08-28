8ccbf8547f3f75238bfd1b1b21282976588f8b6e
Yamashita, Yuu <peek824545201@gmail.com>
2015-08-28 09:06:21 +0900

    use `Gem::Version` to compare version string properly

--- libraries/version.rb
+++ libraries/version.rb
@@ -1,3 +1,5 @@
+require "rubygems"
+
 module TdAgent
   module Version
     def major
@@ -8,23 +10,15 @@ module TdAgent
       @major
     end
 
-    def version_info
-      unless @version_info
-        version = node["td_agent"]["version"]
-        if version.nil?
-          @version_info = [0, 0, 0]
-        else
-          @version_info = [0, 0, 0].zip(version.to_s.split(".", 3).map { |x| x.to_i }).map { |x, y| y || x }
-        end
-      end
-      @version_info
-    end
-
     def reload_available?
       case node["platform_family"]
       when "debian"
         # td-agent's init script for debian starts supporting reload starting from 2.1.4
-        [2, 1, 4].zip(version_info).all? { |x, y| x <= y }
+        if node["td_agent"]["version"]
+          ::Gem::Version.new("2.1.4") <= ::Gem::Version.new(node["td_agent"]["version"].to_s)
+        else
+          false
+        end
       when "redhat"
         # td-agent's init script for redhat is supporting reload
         true
