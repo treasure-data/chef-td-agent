e39d567a6d5c94d066b75ae08ddc4bf06fdfaa41
Maxim Darii <mdarii@tacitknowledge.com>
2016-06-20 14:18:03 +0300

    Added option to use custom templates for source provider

--- resources/source.rb
+++ resources/source.rb
@@ -26,3 +26,4 @@ attribute :source_name, :kind_of => String, :name_attribute => true, :required =
 attribute :type, :kind_of => String, :required => true
 attribute :tag, :kind_of => String
 attribute :params, :kind_of => Hash
+attribute :template_source, :kind_of => String, default: 'td-agent'
