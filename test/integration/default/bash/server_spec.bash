fa6b7bbbfd18ff9b09c4b97767b5e2ef5de20e1e
Yamashita, Yuu <peek824545201@gmail.com>
2017-08-09 06:09:33 +0000

    Logging test results on console

--- test/integration/default/bash/server_spec.bash
+++ test/integration/default/bash/server_spec.bash
@@ -6,4 +6,4 @@ unset GEM_PATH
 export PATH="/opt/chef/embedded/bin:/opt/chef/bin:${PATH}"
 cd "${BUSSER_ROOT}/suites/bash"
 bundle install --path=vendor/bundle
-env SPEC_OPTS="--format=RspecJunitFormatter --out=${BUSSER_ROOT}/result.xml --require=rspec_junit_formatter" bundle exec rake spec
+env bundle exec rake spec
