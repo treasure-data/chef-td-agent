acea1f0299a38317d3a3c5a2025653d7e0293272
Yamashita, Yuu <peek824545201@gmail.com>
2018-02-23 00:42:18 +0000

    Bump version; 3.0.2 [ci skip]

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "k@treasure-data.com"
 license          "Apache 2.0"
 description      "Installs/Configures td-agent"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "3.0.1"
+version          "3.0.2"
 recipe           "td-agent", "td-agent configuration"
 
 chef_version     ">= 12" if respond_to?(:chef_version)
